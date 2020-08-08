84d4a9b51e97bb0002ebe663821985263305a570
Tejas Patel <tejas.patel@dal.ca>
2020-08-08 17:13:54 -0300

    Added products UI, login, proceed to order UIs and controller to authenticate, proceed and finally order.

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,9 +1,4 @@
-/**
- * UserController
- *
- * @description :: Server-side actions for handling incoming requests.
- * @help        :: See https://sailsjs.com/docs/concepts/actions
- */
+
 
 module.exports = {
   
@@ -23,9 +18,44 @@ module.exports = {
         var password=req.body.password;
         console.log("Logged in user's email-id is--->"+emailid);
         res.view('pages/login');
-    }
+    },
+    
+    loginPage:function(req,res)
+    {
+        console.log("Redirect to login page to authenticate user.");
+        console.log(req.body);
+        let order = {
+            sellerId: req.body.sellerId,
+            orderQty: req.body.orderQty,
+            productId: req.body.productId,
+            orderTotal: req.body.orderTotal,
+        }
+        order.orderTotal = order.orderTotal * order.orderQty;
+        res.view('pages/login',{
+            order: order
+        });
+    },
+
+    orderLogin:function(req,res)
+    {
+        let emailid=req.body.email;
+        let password=req.body.password;
 
 
+        let order = {
+            userId: req.body.userId,
+            sellerId: req.body.sellerId,
+            orderQty: req.body.orderQty,
+            productId: req.body.productId,
+            userAdd: req.body.userAdd,
+            orderTotal: req.body.orderTotal,
+        }
+        //authenticate user and set userId and userAdd to order object.
 
+        
+        res.view('pages/proceedOrder', {
+            order: order
+        });
+    },
 };
 
